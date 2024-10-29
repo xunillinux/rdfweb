eadea75d295114c194e3eaf27a7b2ea058651288
xunillinux <jonas@meise.ch>
2024-10-29 15:05:31 +0100

    added launch and vscode settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	//TODO implement
+</script>
+
+<h1>Robot Dog Funeral</h1>
+
+Welcome to our website.
